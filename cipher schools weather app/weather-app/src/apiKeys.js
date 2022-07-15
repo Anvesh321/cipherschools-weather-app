a8e6931261fed89d35f5a933bbdecadb27ab32b2
Anvesh321 <87032810+Anvesh321@users.noreply.github.com>
2022-07-15 08:16:30 +0530

    Add files via upload

---
+++ cipher schools weather app/weather-app/src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{82bc9de97a2258faefd84986a5df5d22}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
